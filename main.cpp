103ceb72afd5617bce2c4b4a12a31f3b02fdcfa2
AlexanderYarushin <sasha-yarushin@mail.ru>
2021-01-07 16:32:43 +0500

    First success runnable

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 #define SPACE 0x20
+#define PROGRAM_END 0xFF
 
 struct Label {
     int adress;
@@ -17,6 +18,12 @@ struct Label {
 };
 
 vector<Label> labelStore;
+
+enum Sign {
+    Equal = 0,
+    Positive,
+    Negative
+};
 
 enum EFlags {
     Number,
@@ -33,7 +40,7 @@ enum Registry {
     RCount,
     R_PC,
     R_COND,
-
+    RCountAll
 };
 
 enum Command {
@@ -58,20 +65,21 @@ const string stringRegister[] = { "ax", "bx", "cx" };
 std::map<string, Registry> regAssoc = { { "ax", R_AX }, { "bx", R_BX }, { "cx", R_CX } };
 
 uint8_t memory[1024];
-uint8_t reg[Registry::RCount];
+uint8_t reg[Registry::RCountAll];
 
 void compile(const char* inputFileName, const char* outputFileName);
+void execute(const char* outputFileName);
 
 int main()
 {
     compile("input.vms", "output.vmb");
-
+    execute("output.vmb");
     return 0;
 }
 
 void writeBinary(ofstream& file, const char* data)
 {
-    file.write(data, sizeof(char));
+    file.write(data, sizeof(uint8_t));
 }
 
 template <typename T>
@@ -92,8 +100,6 @@ Command getOpCode(string& s)
     string threeOP = s.substr(0, 3);
     for (int i = 0; i < CCount; ++i) {
         if (twoOP == stringCommand[i] || threeOP == stringCommand[i]) {
-            //   log("getopcode");
-            log(s);
             if (twoOP == stringCommand[i])
                 s = s.substr(3, s.size());
             if (threeOP == stringCommand[i])
@@ -125,6 +131,116 @@ void clearString(string& s)
     }
 }
 
+void memoryPrint()
+{
+    int i = 0;
+    while (true) {
+        cout << (int)memory[i] << " ";
+        if (memory[i] == PROGRAM_END)
+            break;
+        i++;
+    }
+}
+
+void execute(const char* inputFileName)
+{
+    ifstream input(inputFileName, std::ios::in | std::ios::binary);
+
+    string result;
+
+    int i = 0;
+    while (!input.eof()) {
+        char c;
+        input.read((char*)&c, sizeof(uint8_t));
+        memory[i] = c;
+        i++;
+    }
+    memory[i - 1] = PROGRAM_END;
+
+    //memoryPrint();
+
+    cout << "Size: " << i - 1 << " byte" << endl;
+    log("Run:");
+
+    reg[R_PC] = 0;
+    while (true) {
+
+        if (memory[reg[R_PC]] == PROGRAM_END)
+            break;
+
+        uint8_t opCode = memory[reg[R_PC]];
+        uint8_t flag = memory[reg[R_PC] + 1];
+        uint8_t leftOp = memory[reg[R_PC] + 2];
+        uint8_t rightOp = memory[reg[R_PC] + 3];
+
+        switch (opCode) {
+        case MOV: {
+            if (flag == Number) {
+                reg[leftOp] = rightOp;
+            } else {
+                reg[leftOp] = reg[rightOp];
+            }
+            reg[R_PC] += 4;
+            break;
+        }
+        case ADD: {
+            if (flag == Number) {
+                reg[leftOp] += rightOp;
+            } else {
+                reg[leftOp] += reg[rightOp];
+            }
+
+            reg[R_PC] += 4;
+            break;
+        }
+        case LOG: {
+
+            log((short)reg[leftOp]);
+
+            reg[R_PC] += 4;
+            break;
+        }
+        case CMP: {
+            if (flag == Number) {
+                if (reg[leftOp] == rightOp)
+                    reg[R_COND] = Equal;
+                if (reg[leftOp] < rightOp)
+                    reg[R_COND] = Negative;
+                if (reg[leftOp] > rightOp)
+                    reg[R_COND] = Positive;
+
+            } else {
+                reg[R_CX] = leftOp - reg[rightOp];
+            }
+            reg[R_PC] += 4;
+
+            break;
+        }
+
+        case JN: {
+            if (reg[R_COND] == Negative) {
+                reg[R_PC] = leftOp;
+            } else {
+                reg[R_PC] += 4;
+            }
+            break;
+        }
+
+        default:
+            log("Error");
+            break;
+        }
+    }
+}
+
+void writeCommand(ofstream& file, const char* opCode, const char* flag, const char* leftOpr, const char* rightOpr)
+{
+    writeBinary(file, opCode);
+    writeBinary(file, flag);
+    writeBinary(file, leftOpr);
+    writeBinary(file, rightOpr);
+}
+
 void compile(const char* inputFileName, const char* outputFileName)
 {
     string result;
@@ -132,9 +248,6 @@ void compile(const char* inputFileName, const char* outputFileName)
     ofstream output(outputFileName, std::ios::out | std::ios::binary);
 
     int lineNum = 0;
-
-    input.clear();
-    input.seekg(0);
 
     while (getline(input, result)) {
         if (result[result.length() - 1] == ':') {
@@ -176,20 +289,14 @@ void compile(const char* inputFileName, const char* outputFileName)
                 rightOpr = stoi(result);
             }
 
-            writeBinary(output, numToCharP(opCode));
-            writeBinary(output, numToCharP(flag));
-            writeBinary(output, numToCharP(leftOpr));
-            writeBinary(output, numToCharP(rightOpr));
+            writeCommand(output, numToCharP(opCode), numToCharP(flag), numToCharP(leftOpr), numToCharP(rightOpr));
             break;
         }
         case LOG: {
 
             string logMsg = result.substr(0, result.length());
 
-            writeBinary(output, numToCharP(opCode));
-            writeBinary(output, numToCharP(regAssoc[logMsg]));
-            writeBinary(output, numToCharP(Flags[Number]));
-            writeBinary(output, numToCharP(Flags[Number]));
+            writeCommand(output, numToCharP(opCode), numToCharP(flag), numToCharP(regAssoc[logMsg]), numToCharP(Flags[Number]));
             break;
         }
         case JN:
@@ -197,7 +304,6 @@ void compile(const char* inputFileName, const char* outputFileName)
         case JE: {
             int adress = 0;
             string sLabel = result.substr(0, result.length());
-            log(sLabel);
             for (size_t i = 0; i < labelStore.size(); ++i) {
                 if (sLabel == labelStore[i].name) {
                     adress = labelStore[i].adress;
@@ -205,18 +311,18 @@ void compile(const char* inputFileName, const char* outputFileName)
                 }
             }
 
-            writeBinary(output, numToCharP(opCode));
-            writeBinary(output, numToCharP(adress));
-            writeBinary(output, numToCharP(Flags[Number]));
-            writeBinary(output, numToCharP(Flags[Number]));
+            writeCommand(output, numToCharP(opCode), numToCharP(Flags[Number]), numToCharP(adress), numToCharP(Flags[Number]));
             break;
         }
         default:
-            cout << "syntax error" << endl;
-            break;
+
+            log("Syntax error");
+            return;
         }
     }
 
     input.close();
     output.close();
+
+    log("Compile success");
 }
